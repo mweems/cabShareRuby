c4299110945244f470ab8985331504a54fd70185
Matt Weems <mweems@thoughtworks.com>
2014-06-24 18:01:43 -0400

    initial rails build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CabShareRuby::Application.config.secret_key_base = 'a0ea5815354fa1bef2c6d6535c6c4c80411ab5e33a6b85b4abeb73c641c4df513faf7d6c3f3954b0d7ef96c4e6314e675b987d1aaaedd81ee8ee808edc781c70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CabShareRuby::Application.config.session_store :cookie_store, key: '_cabShareRuby_session'
